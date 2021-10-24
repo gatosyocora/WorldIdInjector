309649da3064479ad856aca035810d5bedb26e2c
gatosyocora <gatosyocora36@gmail.com>
2021-10-24 19:08:57 +0900

    関連スクリプトを追加

---
+++ Assets/WorldIdInjector/Editor/BlueprintIdFileCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gatosyocora.WorldIdInjector
+{
+    public class BlueprintIdFileCreator : EditorWindow
+    {
+        public const string BLUEPRINT_ID_FILE_PATH = "Assets/blueprintId.txt";
+
+        private string blueprintId;
+
+        [MenuItem("VRChat SDK/Create Blueprint Id file", priority = 9999)]
+        private static void Open()
+        {
+            GetWindow<BlueprintIdFileCreator>("BlueprintIdFileCreator");
+        }
+
+        private void OnGUI()
+        {
+            blueprintId = EditorGUILayout.TextField("Blueprint Id", blueprintId);
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUI.DisabledGroupScope(string.IsNullOrEmpty(blueprintId)))
+            {
+                if (GUILayout.Button("Create/Update Blueprint Id file"))
+                {
+                    CreateOrUpdateBlueprintId(blueprintId);
+                }
+            }
+        }
+
+        private void CreateOrUpdateBlueprintId(string blueprintId)
+        {
+            if (Directory.Exists(Path.GetDirectoryName(BLUEPRINT_ID_FILE_PATH)))
+            {
+                File.WriteAllText(BLUEPRINT_ID_FILE_PATH, blueprintId);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
---
+++ Assets/WorldIdInjector/Editor/WorldIdInjector.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using VRC.Core;
+using VRC.SDKBase;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+namespace Gatosyocora.WorldIdInjector
+{
+    public class WorldIdInjector : IVRCSDKBuildRequestedCallback
+    {
+        public int callbackOrder => -1;
+
+        public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+        {
+            if (ExistBlueprintIdFile())
+            {
+                var pipelineManager = GetPilelineManager();
+                var blueprintid = GetBlueprintId();
+                pipelineManager.blueprintId = blueprintid;
+            }
+
+            return true;
+        }
+
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            EditorApplication.playModeStateChanged += (state) =>
+            {
+                if (state == PlayModeStateChange.EnteredEditMode)
+                {
+                    var pipelineManager = GetPilelineManager();
+                    pipelineManager.blueprintId = string.Empty;
+                    EditorUtility.SetDirty(pipelineManager);
+                    EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+                }
+            };
+        }
+
+        private static PipelineManager GetPilelineManager()
+        {
+            return SceneManager.GetActiveScene()
+                .GetRootGameObjects()
+                .SelectMany(rootGameObject => rootGameObject.GetComponentsInChildren<VRC_SceneDescriptor>())
+                .Single()
+                .GetComponent<PipelineManager>();
+        }
+
+        private static bool ExistBlueprintIdFile()
+            => File.Exists(BlueprintIdFileCreator.BLUEPRINT_ID_FILE_PATH);
+
+        private static string GetBlueprintId()
+        {
+            return File.ReadAllText(BlueprintIdFileCreator.BLUEPRINT_ID_FILE_PATH);
+        }
+    }
+}
